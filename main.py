016204207a96e1a66877466ec5724ab701b40a08
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-30 19:09:06 +0700

    Using Python List slice to get the n-first elements from a list

--- main.py
+++ main.py
@@ -11,3 +11,14 @@ if __name__ == "__main__":
   print(colors)
   sub_colors = colors[1:4]
   print(sub_colors)
+
+  # Using Python List slice to get the n-first elements from a list
+
+  """
+  lists[:n]
+  """
+
+  colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+  print(colors)
+  sub_colors = colors[:3]
+  print(sub_colors)
