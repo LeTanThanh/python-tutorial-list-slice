4eaf29bb8904d939b305f085dc6ecdba4eb1faca
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-30 19:06:50 +0700

    Basic Python list slice example

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ if __name__ == "__main__":
   """
   sub_list = list[begin: end: step]
   """
+
+  # Basic Python list slice example
+
+  colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+  print(colors)
+  sub_colors = colors[1:4]
+  print(sub_colors)
